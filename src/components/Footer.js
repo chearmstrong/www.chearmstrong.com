8a83c187c6179a192cec43eb790f51c43f7f617e
Ché Armstrong <che.armstrong@purepoint.io>
2018-09-25 16:16:48 +0100

    Add some initial content (#1)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import { aboutMe } from '../data/content'
+
 const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
-        <p className="copyright">&copy; Gatsby Starter - Dimension. Design: <a href="https://html5up.net">HTML5 UP</a>. Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js</a></p>
+        <p className="copyright">&copy; {aboutMe.name}</p>
     </footer>
 )
 
---
+++ src/data/content.js
@@ -0,0 +1,52 @@
+import React from 'react'
+
+export const social = [
+  {
+    name: 'Twitter',
+    url: 'https://twitter.com/CheArmstrong',
+    icon: 'icon fa-twitter'
+  },
+  {
+    name: 'LinkedIn',
+    url: 'https://www.linkedin.com/in/chearmstrong/',
+    icon: 'icon fa-linkedin'
+  },
+  {
+    name: 'GitHub',
+    url: 'https://github.com/chearmstrong',
+    icon: 'icon fa-github'
+  },
+  {
+    name: 'Stack Overflow',
+    url: 'https://stackoverflow.com/story/chearmstrong',
+    icon: 'icon fa-stack-overflow'
+  }
+]
+
+export const aboutMe = {
+  name: 'Ché Armstrong',
+  intro: "I'm an experienced full-stack software engineer with strong background in JavaScript - I'm passionate about technology, customer-focused and enjoy a challenge. I love finding simple solutions to complex problems, and enjoy learning new skills to help me do that.",
+  skills: [
+    'JavaScript (ES6/ES7/next)',
+    'Node.js',
+    'Amazon Alexa (smarthome & custom)',
+    'Google Home (smarthome)',
+    'Alexa Skills Kit (ASK) SDK',
+    'AWS (including Lambda, S3, EC2, SQS)',
+    'IBM CoudWhisk (Actions)',
+    'Serverless framework',
+    'Microsoft BotBuilder',
+    'Angular.js',
+    'React',
+    'MongoDB',
+    'Git'
+  ]
+}
+
+export const current = (
+  <div>
+  <p>I'm currently the Technical Lead on the Voice Team at <a href="http://purepoint.io" target="_blank" rel="noopener noreferrer">Purepoint</a>, working on Amazon Alexa and Google Home integrations for <a href="http://hivehome.com" target="_blank" rel="noopener noreferrer">Hive</a>'s smart home products.</p>
+  <p>As well as writing code, my day to day tasks include overseeing architecture, making key technical decisions, and attending weekly meetings with Amazon and Google.</p>
+  <p>In addition to this, I also contribute to architectural and technical decisions for other Purepoint projects.</p>
+  </div>
+)
