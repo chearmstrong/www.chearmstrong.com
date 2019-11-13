39f6c9fd06a26983d956c80932ea6306ca6a382f
Che Armstrong <neuroscience.che@gmail.com>
2019-11-13 10:52:05 +0000

    Add sub-pages

--- src/templates/voiceProjectsTemplate.js
+++ src/templates/voiceProjectsTemplate.js
@@ -1,7 +1,10 @@
 /**
- * Tried to do this page using a Function Component and React Hooks.
- * The template uses Class Components for other pages - may look to
- * update these in the future. Still a WIP.
+ * Tried to do this page using a Function Component and (basic) React Hooks
+ * rather than using a Class like the main index page.
+ *
+ * This is a template page - pages for each voice project is created programatically.
+ *
+ * Still a learning curve/WIP.
  */
 
 import { graphql } from 'gatsby'
@@ -10,13 +13,12 @@ import Markdown from 'markdown-to-jsx'
 import Layout from '../components/layout'
 import React, { useState, useEffect } from 'react'
 import mapAboutMeData from '../utils/map-about-me-data'
-import mapMovieCriticData from '../utils/map-movie-critic-data'
-import * as movieCriticLogo from '../images/MovieCritic_no_BG.png'
+import mapVoiceProjectData from '../utils/map-voice-project-data'
 
-const MovieCriticPage = props => {
+const VoiceProjectPage = (props) => {
   const [loading, setLoading] = useState(true)
   const { name } = mapAboutMeData(props)
-  const { title, description } = mapMovieCriticData(props)
+  const { title, description, logo } = mapVoiceProjectData(props)
 
   useEffect(() => {
     setLoading(false)
@@ -30,13 +32,13 @@ const MovieCriticPage = props => {
             <header id="header">
               <div className="content">
                 <div className="inner">
-                  <img src={movieCriticLogo} alt={title} />
+                  <img src={logo} alt={title} />
                 </div>
               </div>
             </header>
             <div
               id="main"
-              className="movie-critic"
+              className="voice-project"
               style={!loading ? { display: 'flex' } : { display: 'none' }}
             >
               <div className="content-box">
@@ -54,28 +56,24 @@ const MovieCriticPage = props => {
               </p>
             </footer>
           </div>
-          <div id="bg-movie-critic" />
+          <div id="bg-voice-project" />
         </div>
       </Layout>
   )
 }
 
-export default MovieCriticPage
+export default VoiceProjectPage
 
 // PAGE QUERY
 export const pageQuery = graphql`
-  query MovieCriticContent {
-    allContentfulMovieCritic {
-      edges {
-        node {
-          id
-          icon
-          title
-          description {
-            description
-          }
-        }
-      }
+  query voiceProjectsQuery($slug: String!) {
+    contentfulMovieCritic(slug: { eq: $slug }) {
+      id
+      icon
+      title
+      description { description }
+      logo { file { url } }
+      slug
     }
     allContentfulAboutMe {
       edges {
